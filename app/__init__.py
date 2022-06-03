0c4b12ea3b6dd01bc40cc9c53ddaa1eddf1fe0a1
juanprograma <69698438+juanprograma@users.noreply.github.com>
2022-06-02 23:01:45 -0500

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import flask
+app = Flask (__name__)
+
+
+def create_app ():
+    app = FLASK (__NAME)
+    app.config.from_object (config)
+    return app
+    
---
+++ app/config.py
@@ -0,0 +1,4 @@
+class config:
+    SECRET_KEY = 'ideas'
+    SQLACHEMY_DATA_URI = 'sqlite:///../ideas.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = FALSE
