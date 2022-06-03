6d08be0a489cbf43247bed060c6ee0e9e0ff4030
dpulidonuniandes <69642387+dpulidonuniandes@users.noreply.github.com>
2022-06-02 23:53:49 -0500

    oootra cosa video 2

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from .database import db
 
 def create_app():
     """Método para la creación de la app de Flask."""
-    app = Flask(name)
+    app = Flask(__name__)
     app.config. from_object(Config)
     db.init_app(app)
     return app
